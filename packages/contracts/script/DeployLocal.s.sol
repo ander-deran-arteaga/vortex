ea4ca34a1accd06c04f491bad6bb0cc88110b4dc
ander-deran-arteaga <ander.arteaga-contijoch@epitech.eu>
2026-07-25 16:37:24 +0200

    feat: deploy vortex swap stack in local script

--- packages/contracts/script/DeployLocal.s.sol
+++ packages/contracts/script/DeployLocal.s.sol
@@ -5,9 +5,14 @@ import { Script } from "forge-std/Script.sol";
 import { VmSafe } from "forge-std/Vm.sol";
 
 import { Aqua } from "@1inch/aqua/src/Aqua.sol";
+import { IAqua } from "@1inch/aqua/src/interfaces/IAqua.sol";
 import { AquaSwapVMRouter } from "@1inch/swap-vm/src/routers/AquaSwapVMRouter.sol";
 
+import { VortexAquaLens } from "../src/aqua/VortexAquaLens.sol";
+import { VortexAquaOrderBuilder } from "../src/aqua/VortexAquaOrderBuilder.sol";
+import { VortexAquaPricing } from "../src/aqua/VortexAquaPricing.sol";
 import { MockERC20 } from "../src/mocks/MockERC20.sol";
+import { MockReferenceOracle } from "../src/mocks/MockReferenceOracle.sol";
 import { MockUSDC } from "../src/mocks/MockUSDC.sol";
 import { MockWBTC } from "../src/mocks/MockWBTC.sol";
 
@@ -25,6 +30,13 @@ contract DeployLocal is Script {
         AquaSwapVMRouter router =
             new AquaSwapVMRouter(address(aqua), address(weth), msg.sender, "AquaSwapVMRouter", "1.0.1");
 
+        // Vortex Swap stack (Phase 2).
+        MockReferenceOracle oracle = new MockReferenceOracle(msg.sender);
+        oracle.setPrice(100_000e18, 99_950e18, 100_050e18);
+        VortexAquaPricing pricing = new VortexAquaPricing(address(router), IAqua(address(aqua)));
+        VortexAquaOrderBuilder orderBuilder = new VortexAquaOrderBuilder(pricing);
+        VortexAquaLens lens = new VortexAquaLens(IAqua(address(aqua)), address(router), pricing);
+
         vm.stopBroadcast();
 
         string memory root = "deployment";
@@ -34,6 +46,10 @@ contract DeployLocal is Script {
         vm.serializeAddress(contracts, "MockWBTC", address(wbtc));
         vm.serializeAddress(contracts, "MockUSDC", address(usdc));
         vm.serializeAddress(contracts, "MockWETH", address(weth));
+        vm.serializeAddress(contracts, "MockReferenceOracle", address(oracle));
+        vm.serializeAddress(contracts, "VortexAquaPricing", address(pricing));
+        vm.serializeAddress(contracts, "VortexAquaOrderBuilder", address(orderBuilder));
+        vm.serializeAddress(contracts, "VortexAquaLens", address(lens));
         string memory contractsJson = vm.serializeAddress(contracts, "AquaSwapVMRouter", address(router));
         string memory json = vm.serializeString(root, "contracts", contractsJson);
 
